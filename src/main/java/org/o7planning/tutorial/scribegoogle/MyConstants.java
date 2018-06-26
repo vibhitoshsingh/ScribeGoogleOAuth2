e9d5491390d8f5856f02735cb1c82acb161ae88f
vibhi <vibhitosh12@gmail.com>
2018-06-26 16:28:28 +0530

    initial commit

---
+++ src/main/java/org/o7planning/tutorial/scribegoogle/MyConstants.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+//package org.o7planning.tutorial.scribegoogle;
+
+/**
+ * @author Admin2
+ *
+ */
+//public class MyConstants {
+
+	/**
+	 * @param args
+	 */
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//}
+
+//}
+
+package org.o7planning.tutorial.scribegoogle;
+
+public class MyConstants {
+ 
+   // Client ID
+   public static final String GOOGLE_CLIENT_ID = "884814088321-lfgc199ui38csrv75bf2fr3etksv0kpm.apps.googleusercontent.com";
+ 
+   // Client Secret
+   public static final String GOOGLE_CLIENT_SECRET = "yNHBZRDB-ThCIRa29pNJEyh-";
+ 
+   // Redirect URI
+   public static final String GOOGLE_REDIRECT_URL = "https://examplewebsite.com/mypath/oauth2callback";
+ 
+}
